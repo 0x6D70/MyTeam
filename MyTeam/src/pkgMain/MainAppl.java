02ffbf2a0b9fa3908623822a97af543023d506a7
0x6D70 <43414123+0x6D70@users.noreply.github.com>
2021-06-18 11:21:01 +0200

    Update MainAppl.java

--- MyTeam/src/pkgMain/MainAppl.java
+++ MyTeam/src/pkgMain/MainAppl.java
@@ -8,5 +8,9 @@ public class MainAppl {
 		Player player = new Player(2, "Dragovic", "Defense");
 		
 		System.out.println("== " + player);
+		
+		Player player2 = new Player(4, "Hinteregger", "Defense");
+		
+		System.out.println("== " + player2);
 	}
 }
